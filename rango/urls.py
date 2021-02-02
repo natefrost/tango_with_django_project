5da76fbd342ec4fbbb60aa100f75df44b81f14b0
Minh Duc Pham <2439753P@student.gla.ac.uk>
2021-02-02 04:05:23 +0100

    commit after exercises chapter 3

--- rango/urls.py
+++ rango/urls.py
@@ -4,7 +4,7 @@ from rango import views
 app_name = "rango"
 
 urlpatterns = [
-    path('', views.index, name = "index")    
-    
+    path('', views.index, name = "index"),    
+    path('about', views.about, name = "about"),
 ]
 
